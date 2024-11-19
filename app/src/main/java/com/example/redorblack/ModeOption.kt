f1889b0c73deb027d46943c5ba7163fb252a310e
beles <beles.dev@gmail.com>
2024-11-19 20:18:13 +0100

    Name changed cus of Sonarlint

---
+++ app/src/main/java/com/example/redorblack/ModeOption.kt
@@ -0,0 +1,27 @@
+package com.example.redorblack
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.redorblack.databinding.ActivityModeOptionBinding
+
+class ModeOption : AppCompatActivity() {
+
+    lateinit var binding: ActivityModeOptionBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivityModeOptionBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
